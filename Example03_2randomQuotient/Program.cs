eb67316d4007a54cadf9dd37a40234827d681816
Maxim Gostry <rotoff7@mail.ru>
2023-01-12 16:25:37 +0300

    add comments for Example03

--- Example03_2randomQuotient/Program.cs
+++ Example03_2randomQuotient/Program.cs
@@ -1,5 +1,7 @@
 ﻿double number1 = new Random().Next(-10, 101);
 double number2 = new Random().Next(-10, 101);
+// Данная команда генерирует рандомные числа в следующем диапазоне [-10; 101)
+// Включая в диапазон -10 и не включая 101, то  есть мы можем получить любое число от -10 до 100!
 double result = (number1 / number2);
 Console.Write("Первое рандомное число: ");
 Console.WriteLine(number1);
