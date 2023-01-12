e3eb31c139882d4bbf9eccf68fd52c26deb4ee37
Maxim Gostry <rotoff7@mail.ru>
2023-01-12 15:42:25 +0300

    sum of 2 random numbers

---
+++ Example02_2randomCount/Program.cs
@@ -0,0 +1,12 @@
+﻿Random rnd = new Random();
+int number1 = rnd.Next(-10, 101);
+int number2 = rnd.Next(-10, 101);
+int sum = number1+number2;
+Console.Write("number 1 is: ");
+Console.WriteLine(number1);
+Console.Write("number 2 is: ");
+Console.WriteLine(number2);
+Console.Write("Сумма двух чисел = ");
+Console.WriteLine(sum);
+// Random rnd = new Random(); - Создание объекта для генерации чисел, обращаясь к которому через rnd.Next(Диапазон), мы будем помещать рандомное число в переменную.
+// Без созданного объекта для генерации рандомных чисел, можно напрямую задать ее через: int numberX = new Random.Next(Диапазон);
