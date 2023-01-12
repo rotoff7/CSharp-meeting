59099a7b48a4f8fcea80019f04b78f2ef4f821db
Maxim Gostry <rotoff7@mail.ru>
2023-01-12 14:13:13 +0300

    Hello to user

---
+++ Example01_HelloConsole/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+Console.Write("Здравствуй "); // Как сделать вывод текста + переменную в одной строке кода?
+Console.WriteLine(username);
