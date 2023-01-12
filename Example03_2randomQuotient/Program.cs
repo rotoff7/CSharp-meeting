864a1aa970a65debf6af9ef63da003c1ffa7f0a0
Maxim Gostry <rotoff7@mail.ru>
2023-01-12 16:11:57 +0300

    a/b, when a,b is random numbers

---
+++ Example03_2randomQuotient/Program.cs
@@ -0,0 +1,12 @@
+﻿double number1 = new Random().Next(-10, 101);
+double number2 = new Random().Next(-10, 101);
+double result = (number1 / number2);
+Console.Write("Первое рандомное число: ");
+Console.WriteLine(number1);
+Console.Write("Второе рандомное число: ");
+Console.WriteLine(number2);
+Console.Write("Результат от деления первого числа на второе = ");
+Console.WriteLine(result);
+// Или же можно не создавать переменную result а в выводе сделать вместо Console.WriteLine(result):
+//Console.WriteLine(number1 / number2);
+
