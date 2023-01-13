3859c492866cbf15945dd9da3560f8bc2f930158
Maxim Gostry <rotoff7@mail.ru>
2023-01-13 11:23:45 +0300

    problem with numberswap

--- Example05_Kettlebell task/Program.cs
+++ Example05_Kettlebell task/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите размер массива: ");
 string SizeFromConsole = Console.ReadLine();
-int size = int.Parse(SizeFromConsole); // Перевод типа данных "string" в "int"
+int size = int.Parse(SizeFromConsole); // Перевод типа данных из "string" в "int"
 int[] numbers = new int[size];
 int index = 0;
 while (index<size)
@@ -8,6 +8,9 @@ while (index<size)
     numbers[index] = new Random().Next(1, 100);
     index = index + 1;
 }
+var massive = string.Join(" ", numbers); //Этот момент не понятен
+Console.WriteLine("Получившийся исходный массив: ");
+Console.WriteLine(massive);
 int max = 0;
 int max_index = 0;
 index = 0;
@@ -17,16 +20,21 @@ while (index<size)
     {
         max = numbers[index];
         max_index = index;
-        index = index + 1;
     }
-    else
-    {
         index = index + 1;
-    }
 }
+
 int help = numbers[size - 1];
 numbers[size - 1] = max;
 numbers[max_index] = help;
-var str = string.Join(" ", numbers); //Этот момент не понятен
+
+
+
+Console.Write("Максимальное число в массиве: ");
 Console.WriteLine(max);
-Console.WriteLine(str);
+Console.WriteLine("Результативный массив: ");
+Console.WriteLine(massive);
+
+// int help = numbers[size - 1];
+// numbers[size - 1] = max;
+// numbers[max_index] = help;
