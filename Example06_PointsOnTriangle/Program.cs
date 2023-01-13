9bbc7e0fdf637c129a9a16f89c17a70d7793d347
Maxim Gostry <rotoff7@mail.ru>
2023-01-13 17:04:46 +0300

    solved?

--- Example06_PointsOnTriangle/Program.cs
+++ Example06_PointsOnTriangle/Program.cs
@@ -1,12 +1,10 @@
 ﻿Console.Clear();
 
-int xa = 2, ya = 1,
+int xa = 2, ya = 2,
 xb = 2, yb = 30,
 xc = 100, yc = 30;
 
-Console.Write("Введите кол-во точек: ");
-int count = Console.Read();
-
+int count = 0;
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 Console.SetCursorPosition(xb, yb);
@@ -14,30 +12,29 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-int x = xa, y = ya;
+int x = xa, y = xb;
 
-int index = 0;
-while (index < count)
+while (count<20000)
 {
-    int help = new Random().Next(0, 3);
+    int help = new Random().Next(0,3);
     if (help == 0)
     {
-    x = (x + xa) / 2;
-    y = (y + ya) / 2;
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
     }
     if (help == 1)
     {
-    x = (x + xb) / 2;
-    y = (y + yb) / 2;
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
     }
     if (help == 2)
     {
-    x = (x + xc) / 2;
-    y = (x + yc) / 2;
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
     }
-Console.SetCursorPosition(x, y);
-Console.WriteLine("+");
-index = index + 1;
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
 }
 
 
