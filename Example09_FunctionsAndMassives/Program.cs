74b5f72833e6731e3dbcf56f006ba416e5c79478
Maxim Gostry <rotoff7@mail.ru>
2023-01-17 16:26:13 +0300

    unreal Example???

---
+++ Example09_FunctionsAndMassives/Program.cs
@@ -0,0 +1,41 @@
+﻿// сравнение введенного числа с имеющейся через функцию
+// 3 массива (5 ячеек, 7 ячеек, 9 ячеек) и максимальное
+
+Console.WriteLine("Кодовое число");
+int size = Convert.ToInt32(Console.ReadLine());
+int length;
+int max = 0;
+int col[] = new int [size];
+void FillArray(int size)
+{
+    if (size == 1)
+        length = 5;
+    else if (size == 2)
+        length = 7;
+    else if (size == 3)
+        length = 9;
+    else
+        length = 0;
+    int index = 0;
+    while (index < length)
+    {
+        col[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+int Max(int arg1)
+
+
+{
+    int index = 0;
+    while (index < length)
+    {
+        if (Array[index] > max)
+            max = Array[index];
+        index++;
+    }
+    return max;
+}
+
+var massive = string.Join(" ", col);
