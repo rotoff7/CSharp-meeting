7f7d58a23d1b6eaa429d3f6a9f066e5fea5ad783
Maxim Gostry <rotoff7@mail.ru>
2023-01-20 11:59:18 +0300

    Example10 in progress

---
+++ Example10_BonusTask1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Создание массива, проверка его на сумму всех значений, если сумма больше 100, 
+// то сортировка от меньшего к большому, если нет, то выдает максимальное значение.
+
+void MassiveFiller(int[] massive) // Заполнение массива
+{
+    int index = 0;
+    while (index < massive.Length)
+    {
+        massive[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+int MassiveSum(int[] fullmassive) // Расчет суммы всех чисел массива.
+{
+int index = 0;
+int sum = 0;
+while (index<fullmassive.Length)
+{
+    sum = sum + fullmassive[index];
+    index++;
+}
+return(sum);
+}
+bool SumChecker(int sumnum)
+{
+return sumnum > 100;
+}
+void MassiveSorting(int[] bigmassive)
+{
+int index = 0;
+int maxPos = bigmassive[index];
+int max = bigmassive[index];
+int size = bigmassive.Length;
+int sortsize = bigmassive.Length;
+int count = 0;
+while (count < sortsize)
+{
+while (index<sortsize)
+{
+    if (bigmassive[index] > max)
+    {
+        max = bigmassive[index];
+        maxPos = index;
+    }
+    index++;
+}
+int help = bigmassive[sortsize];
+bigmassive[sortsize] = max;
+bigmassive[maxPos] = help;
+sortsize = sortsize - 1;
+count++;
+}
+}
---
+++ ExampleHelpFolder/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] bigmassive = { 12, 5, 1, 7, 8, 2, 25, 2 };
+int index = 0;
+int size = bigmassive.Length;
+int sortsize = size;
+int count = 0;
+while (count
+ < size)
+{
+    int max = 0;
+    int maxPos = index;
+    while (index < size)
+    {
+        if (bigmassive[index] > max)
+        {
+            max = bigmassive[index];
+            maxPos = index;
+        }
+        index++;
+    }
+    int help = bigmassive[sortsize - 1];
+    bigmassive[sortsize - 1] = max;
+    bigmassive[maxPos] = help;
+    sortsize = sortsize - 1;
+    count++;
+}
+var massive2_0 = string.Join(" ", bigmassive);
+Console.WriteLine(massive2_0);
