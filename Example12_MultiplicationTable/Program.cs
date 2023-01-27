bc363974b9dedbdd3882de78372251ac87a7c519
Maxim Gostry <rotoff7@mail.ru>
2023-01-27 16:01:57 +0300

    Example12_MultilpicationTable

---
+++ Example12_MultiplicationTable/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вывод таблицы умножения от двойки до введенного значения.
+
+void MultiplicationGeneration(int num)
+{
+    for (int i = 2; i <= num; i++)
+    {
+        for (int j = 2; j <= 10; j++)
+        {
+            int multi = i * j;
+            Console.WriteLine($"{i} x {j} = {multi}");
+        }
+        Console.WriteLine("");
+    }
+}
+
+Console.Write("Введите верхнюю границу таблицы умножения: ");
+int number;
+if (int.TryParse(Console.ReadLine(), out number))
+    MultiplicationGeneration(number);
+else Console.WriteLine("Введено некорректное значение. Введите натуральное число.");
