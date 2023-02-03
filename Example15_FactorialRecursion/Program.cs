298aed787bb4584d420d4be1822adfd789cc3ca1
Maxim Gostry <rotoff7@mail.ru>
2023-02-03 15:47:20 +0300

    Example15_FactorialRecursion#1

---
+++ Example15_FactorialRecursion/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int UserInput()
+{
+    int num;
+    if (int.TryParse(Console.ReadLine(), out num)) ;
+    else
+    {
+        Console.WriteLine("Введено некорректное значение.");
+        Environment.Exit(0);
+    }
+    return num;
+}
+
+int NumberFactorial(int number)
+{
+    if (number == 1) return 1;
+    else return number * NumberFactorial(number - 1);
+}
+
+int num = UserInput();
+int result = 1;
+for (int i = 1; i <= num; i++)
+{
+  Console.WriteLine(NumberFactorial(i));
+}
